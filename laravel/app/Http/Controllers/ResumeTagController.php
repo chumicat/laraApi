bbfef660043777365d6f0c1dd834299738141a68
chumicat <russell.tseng@104.com.tw>
2020-04-13 04:09:24 +0000

    delete two form relation function in api controller

--- laravel/app/Http/Controllers/ResumeTagController.php
+++ laravel/app/Http/Controllers/ResumeTagController.php
@@ -18,15 +18,6 @@ class ResumeTagController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -47,17 +38,6 @@ class ResumeTagController extends Controller
     public function show($id)
     {
         return ResumeTag::find($id); 
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
     }
 
     /**
